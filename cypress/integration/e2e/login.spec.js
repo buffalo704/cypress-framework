65d66060360fded5e89e657c5ef3749b941ae350
Sidney So <buffalo704@hotmail.com>
2021-10-26 18:18:05 -0400

    fix broken test

--- cypress/integration/e2e/login.spec.js
+++ cypress/integration/e2e/login.spec.js
@@ -1,6 +1,6 @@
 import { url } from '../../../config'
-import Navbar from ('../../page-objects/components/Navbar')
-import LoginPage from ('../../page-objects/pages/LoginPage')
+import Navbar from '../../page-objects/components/Navbar'
+import LoginPage from '../../page-objects/pages/LoginPage'
 
 describe('Login Failed Test', () => {
   before(function () {
